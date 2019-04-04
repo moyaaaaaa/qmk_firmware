7be4866328b981490d439ebb4b738a0928dc189e
moyaaaaaa <danmakubi@gmail.com>
2019-04-04 21:08:16 +0900

    feat(normal): LOWERに数字キーを追加

--- keyboards/helix/rev2/keymaps/normal/keymap.c
+++ keyboards/helix/rev2/keymaps/normal/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,     KC_I,      KC_O,     KC_P,     KC_BSLASH, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,     KC_K,      KC_L,     KC_SCLN,  KC_ENT,  \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    EISU,    KANA,    KC_N,    KC_M,     KC_COMMA,  KC_DOT,   KC_SLASH, KC_RSFT, \
-      ADJUST,  KC_GESC, KC_LALT, KC_LGUI, KC_BSPC, LOWER,   KC_SPC,  KC_RGUI, RAISE,   KC_GESC,  KC_QUOTE,    KC_QUOTE, KC_MINUS, KC_EQUAL \
+      KC_LALT, KC_LALT, KC_LALT, KC_LGUI, KC_BSPC, LOWER,   KC_SPC,  KC_RGUI, RAISE,   KC_GESC,  KC_QUOTE,  KC_QUOTE, KC_MINUS, KC_EQUAL \
       ),
 
   /* Colemak
@@ -138,8 +138,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_LOWER] = LAYOUT( \
       _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______,                   KC_LPRN, KC_RPRN, KC_MINS, KC_UNDS, _______, _______, \
-      _______, _______, _______, _______, _______, _______,                   KC_LBRC, KC_RBRC, KC_EQL,  KC_PLUS, KC_DOT,  _______, \
+      _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_LPRN, KC_RPRN, KC_MINS, KC_UNDS, _______, _______, \
+      _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_LBRC, KC_RBRC, KC_EQL,  KC_PLUS, KC_DOT,  _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_DOT,  _______, _______, _______, \
       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
       ),
