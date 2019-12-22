c4744bfa46431cf9fa36fdeb1bdd6bef90b5ace2
moyaaaaaa <danmakubi@gmail.com>
2019-12-22 15:42:48 +0900

    なんか変更した

--- keyboards/ergodash/rev2/keymaps/default/keymap.c
+++ keyboards/ergodash/rev2/keymaps/default/keymap.c
@@ -40,10 +40,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_QWERTY] = LAYOUT( \
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______,                        _______, KC_6,    KC_7,   KC_8,     KC_9,    KC_0,    KC_BSPC, \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,                        KC_QUOTE,  KC_Y,    KC_U,   KC_I,     KC_O,    KC_P,    KC_BSLS, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,                        KC_GRV,  KC_Y,    KC_U,   KC_I,     KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    _______,                        _______, KC_H,    KC_J,   KC_K,     KC_L,    KC_SCLN, KC_ENT,  \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    EISU,                           KANA,    KC_N,    KC_M,   KC_COMM,  KC_DOT,  KC_SLSH, KC_RSFT, \
-    _______, KC_LALT, _______, KC_LGUI,          KC_LCTL, LOWER,  KC_SPC,        KC_RGUI, RAISE ,  KC_GESC,         KC_QUOTE, _______, LOWER,   _______  \
+    _______, KC_LALT, _______, KC_LGUI,          KC_BSPC, LOWER,  KC_SPC,        KC_RGUI, RAISE ,  KC_GESC,         KC_QUOTE, _______, LOWER,   _______  \
   ),
 
   /* Lower
